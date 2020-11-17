3dc4ad8755283b813ee054f666c47f024422d5b7
RowenahWambui <suzygracy@gmail.com>
2020-11-17 23:26:25 +0300

    add photos backend API

--- Application/User/CurrentUser.cs
+++ Application/User/CurrentUser.cs
@@ -4,43 +4,47 @@ using Application.Interfaces;
 using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
-using Persistence;
+using System.Linq;
 
 namespace Application.User
 {
     public class CurrentUser
     {
-        public class Query : IRequest<User>{}
-        
-                public class Handler : IRequestHandler<Query,User>{
-        
-                    private readonly UserManager<AppUser> _userManager;
-                    private readonly IJwtGenerator _jwtGenerator;
-                    private readonly IUserAccessor _userAccessor;
-        
-                    public Handler(UserManager<AppUser> userManager, IJwtGenerator jwtGenerator, IUserAccessor userAccessor){
-                        
-                        _userManager = userManager;
+        public class Query : IRequest<User> { }
 
-                        _jwtGenerator = jwtGenerator;
+        public class Handler : IRequestHandler<Query, User>
+        {
 
-                        _userAccessor = userAccessor;
-                    
-                       
-        
-                    }
-        
-                    public async Task<User> Handle(Query request, CancellationToken cancellationToken){
-        
-                       var user = await _userManager.FindByNameAsync(_userAccessor.GetCurrentUsername());
+            private readonly UserManager<AppUser> _userManager;
+            private readonly IJwtGenerator _jwtGenerator;
+            private readonly IUserAccessor _userAccessor;
 
-                       return new User{
-                            DisplayName = user.DisplayName,
-                            Username = user.UserName,
-                            Token = _jwtGenerator.CreateToken(user),
-                            Image = null
-                       };
-                    }
-                }
+            public Handler(UserManager<AppUser> userManager, IJwtGenerator jwtGenerator, IUserAccessor userAccessor)
+            {
+
+                _userManager = userManager;
+
+                _jwtGenerator = jwtGenerator;
+
+                _userAccessor = userAccessor;
+
+
+
+            }
+
+            public async Task<User> Handle(Query request, CancellationToken cancellationToken)
+            {
+
+                var user = await _userManager.FindByNameAsync(_userAccessor.GetCurrentUsername());
+
+                return new User
+                {
+                    DisplayName = user.DisplayName,
+                    Username = user.UserName,
+                    Token = _jwtGenerator.CreateToken(user),
+                    Image = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
+                };
+            }
+        }
     }
 }
--- Infrastructure/Photos/PhotoAccessor.cs
+++ Infrastructure/Photos/PhotoAccessor.cs
@@ -27,8 +27,10 @@ namespace Infrastructure.Photos
 
             if(file.Length > 0){
                 using(var stream = file.OpenReadStream()){
+
                     var uploadParams = new ImageUploadParams{
-                        File = new FileDescription(file.FileName, stream)
+                        File = new FileDescription(file.FileName, stream),
+                        Transformation = new Transformation().Height(500).Width(500).Crop("fill").Gravity("face")
                     };
                     uploadResult = _cloudinary.Upload(uploadParams);
                 }
@@ -45,7 +47,11 @@ namespace Infrastructure.Photos
 
         public string DeletePhoto(string publicId)
         {
-            throw new System.NotImplementedException();
+            var deleteParams = new DeletionParams(publicId);
+
+            var result = _cloudinary.Destroy(deleteParams);
+
+            return result.Result == "ok" ? result.Result : null;
         }
     }
 }
